cdb35e38fa5cf6471a4a3eb4f11466ce6fb8ae4f
Miguel <up202008266@fc.up.pt>
2023-08-02 15:18:45 +0100

    fixed url typo

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Footer from './components/Footer';
 
 function App() {
 
-  const host = "https://141.147.118.36:8443/";
+  const host = "https://141.147.118.36:8443";
 
   const [showNav, setShowNav] = useState(false);
 
