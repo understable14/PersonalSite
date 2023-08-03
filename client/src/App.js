488517489c0ed07b49aa2884799d909dff2264de
understable14 <57635687+understable14@users.noreply.github.com>
2023-08-03 13:02:54 +0100

    if this works...

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Footer from './components/Footer';
 
 function App() {
 
-  //wtf is going on
+  //wtf is going on if this works lmao
   const host = "https://141.147.118.36:8443";
 
   const [showNav, setShowNav] = useState(false);
